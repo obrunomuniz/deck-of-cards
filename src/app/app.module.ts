dbcfca39c303f190de2884f2442e05545139d325
Bruno Muniz <brunomunizdev@gmail.com>
2023-10-07 11:04:44 -0300

    Add component Deck List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxButtonModule } from 'igniteui-angular';
+import { DeckListComponent } from './components/deck-list/deck-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeckListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/deck-list/deck-list.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeckListComponent } from './deck-list.component';
+
+describe('DeckListComponent', () => {
+  let component: DeckListComponent;
+  let fixture: ComponentFixture<DeckListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DeckListComponent]
+    });
+    fixture = TestBed.createComponent(DeckListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
