b5216a1d8650d0320de05a2bef672c0c278d7b8f
Bruno Muniz <brunomunizdev@gmail.com>
2023-10-07 11:10:28 -0300

    Add component DeckDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxButtonModule } from 'igniteui-angular';
-import { DeckListComponent } from './components/deck-list/deck-list.component';
-import { DeckEditComponent } from './components/deck-edit/deck-edit.component';
-
+import { DeckListComponent } from './component/deck-list/deck-list.component';
+import { DeckEditComponent } from './component/deck-edit/deck-edit.component';
+import { DeckDetailComponent } from './component/deck-detail/deck-detail.component';
 @NgModule({
   declarations: [
     AppComponent,
     DeckListComponent,
-    DeckEditComponent
+    DeckEditComponent,
+    DeckDetailComponent
   ],
   imports: [
     BrowserModule,
